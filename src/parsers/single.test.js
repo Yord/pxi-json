ceca9e654685c2d70c608bc43d9ce3b450ed53d3
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-21 12:31:03 +0100

    test: single parser may fail with one of the given list of errors

--- src/parsers/single.test.js
+++ src/parsers/single.test.js
@@ -1,4 +1,4 @@
-const {anything, array, assert, constant, integer, unicodeJsonObject, property} = require('fast-check')
+const {anything, array, assert, constant, integer, unicodeJson, unicodeJsonObject, property} = require('fast-check')
 const {func: parser} = require('./single')
 
 test('parses each json element individually, even if it is formatted over several lines', () => {
@@ -20,6 +20,29 @@ test('parses each json element individually, even if it is formatted over severa
         parser(argv)(tokens, lines)
       ).toStrictEqual(
         {err, jsons}
+      )
+    )
+  )
+})
+
+test('parsing text that is not json throws one of a list of errors, not using lines since verbose is 0', () => {
+  const argv                     = {verbose: 0}
+  const lines                    = anything()
+  const potentiallyInvalidTokens = array(unicodeJson().map(str => str.slice(1)))
+  const msgs                     = [
+    'SyntaxError: Unexpected end of',
+    'SyntaxError: Unexpected token ',
+    'SyntaxError: Unexpected number'
+  ]
+
+  assert(
+    property(lines, potentiallyInvalidTokens, (lines, tokens) =>
+      parser(argv)(tokens, lines)
+      .err
+      .map(e => e.slice(0, 30))
+      .reduce(
+        (bool, err) => bool && msgs.indexOf(err) > -1,
+        true
       )
     )
   )
