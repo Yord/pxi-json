bc07ebed952f8937317bd0f94f8403bd68ecf1ca
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-21 20:44:35 +0100

    test: check if the bulk parser throws exactly one error in contrast to the single parser

--- src/parsers/bulk.test.js
+++ src/parsers/bulk.test.js
@@ -1,4 +1,4 @@
-const {anything, array, assert, constant, integer, unicodeJson, unicodeJsonObject, property} = require('fast-check')
+const {anything, array, assert, constant, func, integer, unicodeJson, unicodeJsonObject, property} = require('fast-check')
 const {func: parser} = require('./bulk')
 
 test('parses json elements as a whole, even if they are formatted over several lines', () => {
@@ -43,6 +43,32 @@ test('parsing text that is not json throws one of a list of errors, not using li
       .reduce(
         (bool, err) => bool && msgs.indexOf(err) > -1,
         true
+      )
+    )
+  )
+})
+
+test('parsing text that is not json fails with exactly one error and the first line if verbose is 1', () => {
+  const argv           = {verbose: 1}
+  const jsons          = []
+  const tokensLinesErr = integer(0, 20).chain(len =>
+    array(integer(), len, len).chain(lines =>
+      array(func(anything()).map(f => f.toString()), len, len).chain(tokens =>
+        constant({
+          tokens,
+          lines,
+          err: lines.length > 0 ? [`(Line ${lines[0]}) SyntaxError: Unexpected token < in JSON at position 1`] : []
+        })
+      )
+    )
+  )
+
+  assert(
+    property(tokensLinesErr, ({tokens, lines, err}) =>
+      expect(
+        parser(argv)(tokens, lines)
+      ).toStrictEqual(
+        {err, jsons}
       )
     )
   )
