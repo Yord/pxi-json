fbfd4a310ce2e49402e4272b64fe927f475b8c08
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-27 12:30:01 +0100

    test: combine jsonBulk and jsonSingle tests into json tests

--- src/parsers/json.test.js
+++ src/parsers/json.test.js
@@ -1,5 +1,5 @@
 const {anything, array, assert, constant, func, integer, unicodeJson, unicodeJsonObject, property} = require('fast-check')
-const {func: parser} = require('./bulk')
+const {func: parser} = require('./json')
 
 test('parses json elements as a whole, even if they are formatted over several lines', () => {
   const err         = []
@@ -85,6 +85,108 @@ test('parsing text that is not json fails with exactly one error, the first line
           tokens,
           lines,
           err: lines.length > 0 ? [{msg: 'Unexpected token < in JSON at position 1', line: lines[0], info: '[' + tokens.join(',') + ']'}] : []
+        })
+      )
+    )
+  )
+
+  assert(
+    property(argv, tokensLinesErr, (argv, {tokens, lines, err}) =>
+      expect(
+        parser(argv)(tokens, lines)
+      ).toStrictEqual(
+        {err, jsons}
+      )
+    )
+  )
+})
+
+test('parses each json element individually, even if it is formatted over several lines', () => {
+  const err         = []
+  const argv        = anything().chain(verbose => constant({verbose, noBulk: true}))
+  const lines       = anything()
+  const jsonsTokens = integer().chain(spaces => 
+    array(unicodeJsonObject()).chain(jsons => 
+      constant({
+        jsons,
+        tokens: jsons.map(json => JSON.stringify(json, null, spaces))
+      })  
+    )
+  )
+
+  assert(
+    property(argv, lines, jsonsTokens, (argv, lines, {jsons, tokens}) =>
+      expect(
+        parser(argv)(tokens, lines)
+      ).toStrictEqual(
+        {err, jsons}
+      )
+    )
+  )
+})
+
+test('parsing text that is not json throws one of a list of errors, not using lines since verbose is 0', () => {
+  const argv                     = {verbose: 0, noBulk: true}
+  const lines                    = anything()
+  const potentiallyInvalidTokens = array(unicodeJson().map(str => str.slice(1)))
+  const msgs                     = [
+    'Unexpected end of',
+    'Unexpected token ',
+    'Unexpected number',
+    'Unexpected string'
+  ]
+
+  assert(
+    property(lines, potentiallyInvalidTokens, (lines, tokens) =>
+      parser(argv)(tokens, lines)
+      .err
+      .map(e => e.msg.slice(0, 17))
+      .reduce(
+        (bool, err) => bool && msgs.indexOf(err) > -1,
+        true
+      )
+    )
+  )
+})
+
+test('parsing text that is not json fails with errors and lines if verbose is 1', () => {
+  const argv           = {verbose: 1, noBulk: true}
+  const jsons          = []
+  const tokensLinesErr = integer(0, 20).chain(len =>
+    array(integer(), len, len).chain(lines =>
+      array(func(anything()).map(f => f.toString()), len, len).chain(tokens =>
+        constant({
+          tokens,
+          lines,
+          err: lines.map(line => ({msg: 'Unexpected token < in JSON at position 0', line}))
+        })
+      )
+    )
+  )
+
+  assert(
+    property(tokensLinesErr, ({tokens, lines, err}) =>
+      expect(
+        parser(argv)(tokens, lines)
+      ).toStrictEqual(
+        {err, jsons}
+      )
+    )
+  )
+})
+
+test('parsing text that is not json fails with errors, lines, and info if verbose is 2 or higher', () => {
+  const argv           = integer(2, 50).chain(verbose => constant({verbose, noBulk: true}))
+  const jsons          = []
+  const tokensLinesErr = integer(0, 20).chain(len =>
+    array(integer(), len, len).chain(lines =>
+      array(func(anything()).map(f => f.toString()), len, len).chain(tokens =>
+        constant({
+          tokens,
+          lines,
+          err: lines.map(
+            (line, index) => ({msg: 'Unexpected token < in JSON at position 0', line, info: tokens[index]})
+          )
         })
       )
     )
